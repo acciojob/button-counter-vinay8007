be0f9cc1bb670a2de38fd89690ffb9320f0f84ed
vinay8007 <161361880+vinay8007@users.noreply.github.com>
2026-01-03 22:39:17 +0530

    Refactor ReactDOM usage for concurrent rendering
    Updated ReactDOM import and rendering method to use createRoot.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
-
+ReactDOM.createRoot(document.getElementById("root)).render(
+      <React.StrickMode>
+            <App/>
+      </React.StrickMode>
+);
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
