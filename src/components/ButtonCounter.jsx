f77056c82cfacdd7353265ee1ef854ab1324799b
vinay8007 <161361880+vinay8007@users.noreply.github.com>
2026-01-03 23:29:47 +0530

    Add ButtonCounter component with click functionality

--- src/components/ButtonCounter.jsx
+++ src/components/ButtonCounter.jsx
@@ -1,1 +1,12 @@
+import { useState } from 'react';
 
+function ButtonCounter(){
+  const [count, setCount] = useState(0);
+  return(
+    <div>
+      <p>Button clicked{count}</p>
+      <button onClick={()=>setCount(count + 1)}>Click Me</button>
+    </div>
+  );
+}
+export default ButtonCounter;
